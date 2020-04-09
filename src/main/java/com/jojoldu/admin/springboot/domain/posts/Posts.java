887a89a16c98bef71929e39453856c3e64c3a690
hwangikjae <hwangikjae@nate.com>
2020-04-09 20:23:48 +0900

    JPA 설정 및 테스트 코드 작성

---
+++ src/main/java/com/jojoldu/admin/springboot/domain/posts/Posts.java
@@ -0,0 +1,39 @@
+package com.jojoldu.admin.springboot.domain.posts;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //클래스 내 모든 필드의 Getter 메소드를 자동 생성
+@NoArgsConstructor  //기본 생성자 자동추가 public Posts(){}와 같은 효과
+@Entity //테이블과 링크될 클래스임을 나타냄.기본값으로 클래스의 카멜케이스 이름을 언더스코어네이밍(_)으로 테이블 이름을 매칭한다.ex)SalesManager.java -> sales_manager table
+public class Posts {
+    @Id //해당 테이블의 PK필드를 나타낸다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성규칙을 나타낸다. 스프링부트 2.0에서는 GenerationType.IDENTITY를 추가해야만 auto_increment가 된다.
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String contents;
+
+    private String author;
+
+    @Builder    //해당 클래스의 빌더 패턴 클래스를 생성. 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String contents, String author){
+        this.title = title;
+        this.contents = contents;
+        this.author = author;
+    }
+    //@Column
+    //테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모드 칼럼이 된다.
+    //사용하는 이유는 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용한다.
+    //문자열의 경우 vachar(255)가 기본값인데 사이즈를 500으로 늘리고 싶거나 타입을 text로 변경하고 싶거나 등의 경우에 사용한다.
+
+    //왠만하면 Entity의 PK는 Long 타입의 Auto_increment를 추천한다.(mysql 기준으로 이렇게 하면 bigint)
+    //주민번호와 같이 비지니스상 유니크한 키나 여러키를 조합한 복합키로 PK를 잡을 경우 난감한 상황이 발생한다.
+}
---
+++ src/main/java/com/jojoldu/admin/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,15 @@
+package com.jojoldu.admin.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+}
+
+//Post 클래스로 데이터베이스를 접근하게 해줄 JPA Reposytory..
+// 보통 ibatis나 mybatis 등에서 dao라고 불리는 db Layer 접근자다.
+// JPA에서는 Reposytory라 부르며 인터페이스로 생성한다.
+// 단순히 인터페이스 생성 후, JpaRepository<클래스명, PK타입>을 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
+// @Repository츨 추가할 필요도 없다.
+//주의점은 Entity 클래스와 기본 Entity Repository는 함께 위치 해야 한다는 점.
+//둘은 아주 밀접한 관계이고 Entity클래스는 기본 repository 없이는 제대로 역할을 할 수 없다.
