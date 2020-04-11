06c542b5023ef434d6507ccd1bcd5b4a110fbfad
hwangikjae <hwangikjae@nate.com>
2020-04-11 20:23:58 +0900

    JPA Auditing으로 생성시간/수정시간 자동화 하기!!!

--- src/main/java/com/jojoldu/admin/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/admin/springboot/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.jojoldu.admin.springboot.domain.posts;
 
 
+import com.jojoldu.admin.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter //클래스 내 모든 필드의 Getter 메소드를 자동 생성
 @NoArgsConstructor  //기본 생성자 자동추가 public Posts(){}와 같은 효과
 @Entity //테이블과 링크될 클래스임을 나타냄.기본값으로 클래스의 카멜케이스 이름을 언더스코어네이밍(_)으로 테이블 이름을 매칭한다.ex)SalesManager.java -> sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id //해당 테이블의 PK필드를 나타낸다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성규칙을 나타낸다. 스프링부트 2.0에서는 GenerationType.IDENTITY를 추가해야만 auto_increment가 된다.
     private Long id;
