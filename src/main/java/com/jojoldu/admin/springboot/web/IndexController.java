c2b0e58c4807511c040436ce50de85a074950434
hwangikjae <hwangikjae@nate.com>
2020-04-13 00:19:05 +0900

    구글 로그인 성공

--- src/main/java/com/jojoldu/admin/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/admin/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.admin.springboot.web;
 
+import com.jojoldu.admin.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.admin.springboot.service.posts.PostsService;
 import com.jojoldu.admin.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +10,25 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){//서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
         //여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달한다.
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");//CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성했다.
+        //즉 로그인 성공시, httpSession.getAttribute("user")에서 값을 가져올 수 있다.
+        if(user != null){   //세션에 저장된 값이 있을 때만 모델에 userName으로 등록한다.
+            //세션에 저장된 값이 없으면 모델에 아무런 값이 없는 상태이므로 로그인 버튼이 보이게 된다!
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
